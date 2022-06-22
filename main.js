8820c1efee9828345cf7fd16385e6e1fc983dc68
Arafat002 <92992303+Arafat002@users.noreply.github.com>
2022-06-22 13:18:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+    let x,y;
+x = parseFloat(parseInt(prompt("enter the first number")));
+y = parseFloat(parseInt(prompt("enter the second number")));
+
+let addition = x+y;
+let subtraction = x-y;
+let multiplication = x*y;
+let Division = x/y;
+
+alert("the sum of the given number is:" + addition);
+alert("the difference of the given number is:" + subtraction);
+alert("the product of the given number is:" + multiplication);
+alert("the quotient of the given number is:" + Division)
